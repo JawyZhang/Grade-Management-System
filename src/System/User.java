c3016869ae01aed6ad96b4ae8752fafb84ae22d1
JawyZhang <44539507+JawyZhang@users.noreply.github.com>
2019-10-29 09:49:42 +0800

    2019/10/27 zw initial

--- src/System/User.java
+++
@@ -1,9 +0,0 @@
-package System;
-
-import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
-
-public class User {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-}
