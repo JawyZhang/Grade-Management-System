7db2f961664ce90f04d8c19177ecc8091f4fb6e0
JawyZhang <44539507+JawyZhang@users.noreply.github.com>
2019-10-31 09:17:28 +0800

    StudentView设置了main函数以供测试

--- src/SystemUI/TeacherView.java
+++ src/SystemUI/TeacherView.java
@@ -1,5 +1,35 @@
 package SystemUI;
 
-public class TeacherView {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TablePosition;
+import javafx.stage.Stage;
 
+import javax.swing.text.TabableView;
+
+public class TeacherView extends Application {
+    @FXML
+    private TabableView tabableView ;
+
+    @FXML
+    private Button btn_ok;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("TeacherView_layout.fxml"));
+        Scene scene = new Scene(root,600,400);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
