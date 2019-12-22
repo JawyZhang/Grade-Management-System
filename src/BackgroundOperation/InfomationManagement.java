8507a198ac65327b18a0d075819573dfb33defba
XiaoLin <949157394@qq.com>
2019-12-22 14:02:59 +0800

    12月20日新增

--- src/BackgroundOperation/InfomationManagement.java
+++ src/BackgroundOperation/InfomationManagement.java
@@ -1,150 +1,46 @@
-package SystemUI;
+package BackgroundOperation;
 
-import DbOperation.DbUtil;
-
-import javax.swing.*;
-
-import javax.swing.border.EmptyBorder;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Vector;
 
-public class AdminView extends JFrame {
-    JButton delete = null;
-    JTable jTable = null;
-    JButton addItem = null;
-    JScrollPane jsb = null;
-    JRadioButton student = null;
-    JRadioButton teacher = null;
-    ButtonGroup bg = null;
-    JPanel navigator = null;
-    JPanel downButton = null;
-    JComboBox c = null;
-    JComboBox college = null;
-    JButton search = null;
-    DefaultTableModel tableModel = null;
-    JButton modify = null;
+import javax.swing.ButtonGroup;
+import javax.swing.DefaultCellEditor;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
-    public AdminView() throws SQLException {
-        navigator = new JPanel();
-        navigator.setLayout(new FlowLayout());
-        student = new JRadioButton("student");
-        teacher = new JRadioButton("teacher");
-        student.setSelected(true);
-        search = new JButton("search");
-        search.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                try {
-                    SearchData();
-                    if (addItem.isEnabled() == false) {
-                        addItem.setEnabled(true);
-                    }
-                    if (delete.isEnabled() == false) {
-                        delete.setEnabled(true);
-                    }
-                    if (modify.isEnabled() == false) {
-                        modify.setEnabled(true);
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        bg = new ButtonGroup();
-        bg.add(student);
-        bg.add(teacher);
-        navigator.add(student);
-        navigator.add(teacher);
+import DbOperation.DbUtil;
 
-        college = new JComboBox();
-        c=new JComboBox();
-        getCollege();
-        navigator.add(college);
-        navigator.add(search);
-
-        addItem = new JButton("add");
-        addItem.setEnabled(false);
-        addItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                addData();
-            }
-        });
-
-        delete = new JButton("delete");
-        delete.setEnabled(false);
-        delete.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                deleteItem();
-            }
-        });
-
-        modify = new JButton("modify");
-        modify.setEnabled(false);
-        modify.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                modifyData();
-            }
-        });
-
-        tableModel = new DefaultTableModel();
-        jTable = new JTable(tableModel);
-        jTable.setBorder(BorderFactory.createLoweredBevelBorder());
-        DefaultTableCellRenderer r = new DefaultTableCellRenderer();
-        r.setHorizontalAlignment(JLabel.CENTER);
-        jTable.setDefaultRenderer(Object.class, r);
-        downButton = new JPanel();
-        downButton.setLayout(new FlowLayout());
-
-        downButton.add(addItem);
-        downButton.add(delete);
-        downButton.add(modify);
-
-        this.setLayout(new BorderLayout());
-        this.add(navigator, BorderLayout.NORTH);
-        jsb = new JScrollPane(jTable);
-        jsb.setBorder(new EmptyBorder(5, 5, 5, 5));
-        this.add(jsb, BorderLayout.CENTER);
-        this.add(downButton, BorderLayout.SOUTH);
-
-        this.setTitle("Administrator:");
-        this.setSize(600, 400);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLocation(200, 200);
-        this.setVisible(true);
-    }
-
-    private void getCollege() throws SQLException {
+public class InfomationManagement {
+	 JButton delete = null;
+	    JTable jTable = null;
+	    JButton addItem = null;
+	    JScrollPane jsb = null;
+	    JRadioButton student = null;
+	    JRadioButton teacher = null;
+	    ButtonGroup bg = null;
+	    JPanel navigator = null;
+	    JPanel downButton = null;
+	    JComboBox c = null;
+	    JComboBox college = null;
+	    JButton search = null;
+	    DefaultTableModel tableModel = null;
+	    JButton modify = null;
+	
+	
+	private void searchCollege() throws SQLException {
         String sql = "SELECT name FROM college";
         ResultSet rs = DbUtil.executeQuery(sql);
-        college.addItem("Select College");
-        try {
-            while (rs.next()) {
-                college.addItem(rs.getString("name"));
-                c.addItem(rs.getString("name"));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        rs.close();
-        DbUtil.close();
-    }
 
-    private void searchCollege() throws SQLException {
-        String sql = "SELECT name FROM college";
-        ResultSet rs = DbUtil.executeQuery(sql);
-        college.addItem("Select College");
+		college.addItem("Select College");
         try {
             while (rs.next()) {
                 college.addItem(rs.getString("name"));
@@ -161,7 +57,7 @@ public class AdminView extends JFrame {
         if (jTable != null) {
             int row = jTable.getSelectedRow();
             String sql = "UPDATE ";//student WHERE id = '1710120009'";
-            sql += "student ";
+                sql += "student ";
 
             sql+="SET";
             for(int i=0;i<jTable.getColumnCount();i++){
@@ -197,7 +93,7 @@ public class AdminView extends JFrame {
             int row = jTable.getSelectedRow();
             String sql = "UPDATE ";//student WHERE id = '1710120009'";
 
-            sql += "teacher ";
+                sql += "teacher ";
 
             sql+="SET";
             for(int i=0;i<jTable.getColumnCount();i++){
@@ -206,7 +102,7 @@ public class AdminView extends JFrame {
             sql = sql.substring(0, sql.length() - 2);
 
 
-            sql += " WHERE (id = '" + jTable.getValueAt(row, 0).toString() + "')";
+                sql += " WHERE (id = '" + jTable.getValueAt(row, 0).toString() + "')";
 
             System.out.println(sql);
             String message = "?Â·??????:";
@@ -232,7 +128,7 @@ public class AdminView extends JFrame {
             }
         }
     }
-
+    
     private void modifyData(){
         if (jTable != null) {
             int row = jTable.getSelectedRow();
@@ -325,8 +221,8 @@ public class AdminView extends JFrame {
         if (tableModel != null) {
             tableModel.setRowCount(0);
             tableModel.setColumnCount(0);
-            tableModel.addColumn("id");
-            tableModel.addColumn("name");
+                tableModel.addColumn("id");
+                tableModel.addColumn("name");
 
             tableModel.addColumn("college");
 
@@ -335,27 +231,27 @@ public class AdminView extends JFrame {
             Vector row = null;
             ResultSet rs = null;
 
-            sql += "teacher ";
-            if (college.getSelectedIndex() != 0) {
-                sql += "WHERE college = '";
-                sql += college.getSelectedItem().toString() + "'";
-            }
-            rs = DbUtil.executeQuery(sql);
-            while (rs.next()) {
-                row = new Vector();
-                row.add(rs.getString("id"));
-                row.add(rs.getString("name"));
-                row.add(rs.getString("college"));
-                if (tableModel != null) {
-                    tableModel.addRow(row);
+                sql += "teacher ";
+                if (college.getSelectedIndex() != 0) {
+                    sql += "WHERE college = '";
+                    sql += college.getSelectedItem().toString() + "'";
                 }
-                row = null;
-                tableModel.addRow(row);
-                jTable.getColumnModel().getColumn(tableModel.getColumnCount()-1).setCellEditor(new DefaultCellEditor(c));
-                rs.close();
-                conn.close();
-                DbUtil.close();
-                tableModel.fireTableDataChanged();
+                rs = DbUtil.executeQuery(sql);
+                while (rs.next()) {
+                    row = new Vector();
+                    row.add(rs.getString("id"));
+                    row.add(rs.getString("name"));
+                    row.add(rs.getString("college"));
+                    if (tableModel != null) {
+                        tableModel.addRow(row);
+                    }
+            row = null;
+            tableModel.addRow(row);
+            jTable.getColumnModel().getColumn(tableModel.getColumnCount()-1).setCellEditor(new DefaultCellEditor(c));
+            rs.close();
+            conn.close();
+            DbUtil.close();
+            tableModel.fireTableDataChanged();
             }
         }
     }
@@ -431,7 +327,7 @@ public class AdminView extends JFrame {
         }
         String sql = "INSERT INTO ";
 
-        sql += "teacher VALUES(?,?,?)";
+            sql += "teacher VALUES(?,?,?)";
 
         Connection conn = DbUtil.getConnection();
         try {
@@ -510,7 +406,7 @@ public class AdminView extends JFrame {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
+        
     }
 
     private  void deleteStudent(){
@@ -544,8 +440,8 @@ public class AdminView extends JFrame {
             int row = jTable.getSelectedRow();
             String sql = "DELETE FROM ";//student WHERE id = '1710120009'";
 
-            sql += "teacher ";
-            sql += "WHERE id = '" + jTable.getValueAt(row, 0).toString() + "'";
+                sql += "teacher ";
+                sql += "WHERE id = '" + jTable.getValueAt(row, 0).toString() + "'";
 
             int n = JOptionPane.showConfirmDialog(null, "???Ã¨?Â¤?? Ã©?Â¤id???" + jTable.getValueAt(row, 0).toString() + "???Ã¨?Â°?????â€”???", "?? Ã©?Â¤Ã©??", JOptionPane.YES_NO_OPTION);
 
@@ -592,19 +488,5 @@ public class AdminView extends JFrame {
                 return;
             }
         }
-    }
-
-    private static String openFile() {//return fileName
-        JFileChooser fDialog = new JFileChooser();
-        fDialog.setDialogTitle("Select File");
-        int returnVal = fDialog.showOpenDialog(null);
-        if (JFileChooser.APPROVE_OPTION == returnVal) {
-            return fDialog.getSelectedFile().toString();
-        } else
-            return null;
-    }
-
-    public static void main(String[] args) throws SQLException {
-        AdminView av = new AdminView();
     }
 }
